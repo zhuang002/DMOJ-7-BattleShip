52ded732d99d9416f56df631d4afeed049929035
zhuang002 <zhuang001@gmail.com>
2018-08-25 01:17:32 -0400

    fix bug

--- src/battleship/BattleShip.java
+++ src/battleship/BattleShip.java
@@ -30,41 +30,24 @@ public class BattleShip {
 
     private static boolean testHorizontal(int i, int j, int l, char[][] b) {
         if (j+l>b.length) return false;
-        int x,y,z;
-        
-        y=j-1;
-        z=l+2;
-        if (y<0) {
-            y=0;
-            z--;
-        }
-        if (y+z>b.length-1) {
-            z--;
-        }
-        x=i-1;
-        if (x>=0)  {
-            for (int k=0;k<z;k++) {
-                if (b[x][y+k]=='h') return false;
+        if (i>0) {
+            if (j>0 && b[i-1][j-1]=='h') return false;
+            for (int k=0;k<l;k++) {
+                if (b[i-1][j+k]=='h') return false;
             }
+            if (j+l<b.length && b[i-1][j+l]=='h') return false; 
         }
-        x++;
-        if (x>=0)  {
-            for (int k=0;k<z;k++) {
-                char c=b[x][y+k];
-                if (k>0 && k<z-1 ) 
-                {
-                    if (c=='m')
-                        return false;
-                }
-                else if (c=='h') 
-                    return false;
+        if (j>0 && b[i][j-1]=='h') return false;
+        if (j+l<b.length && b[i][j+l]=='h') return false;
+        if (i<b.length-1) {
+            if (j>0 && b[i+1][j-1]=='h') return false;
+            for (int k=0;k<l;k++) {
+                if (b[i+1][j+k]=='h') return false;
             }
+            if (j+l<b.length && b[i+1][j+l]=='h') return false; 
         }
-        x++;
-        if (x<b.length)  {
-            for (int k=0;k<z;k++) {
-                if (b[x][y+k]=='h') return false;
-            }
+        for (int k=0;k<l;k++) {
+            if (b[i][j+k]=='m') return false;
         }
         
         return true;
@@ -72,41 +55,24 @@ public class BattleShip {
 
     private static boolean testVertical(int i, int j, int l, char[][] b) {
         if (i+l>b.length) return false;
-        int x,y,z;
-        
-        x=i-1;
-        z=l+2;
-        if (x<0) {
-            x=0;
-            z--;
-        }
-        if (x+z>b.length-1) {
-            z--;
-        }
-        
-        y=j-1;
-        if (y>=0)  {
-            for (int k=0;k<z;k++) {
-                if (b[x+k][y]=='h') return false;
+        if (j>0) {
+            if (i>0 && b[i-1][j-1]=='h') return false;
+            for (int k=0;k<l;k++) {
+                if (b[i+k][j-1]=='h') return false;
             }
+            if (i+l<b.length && b[i+l][j-1]=='h') return false; 
         }
-        y++;
-        if (y>=0)  {
-            for (int k=0;k<z;k++) {
-                char c=b[x+k][y];
-                if (k>0 && k<z-1) {
-                    if (c=='m')
-                        return false;
-                }
-                else if (c=='h') 
-                    return false;
+        if (i>0 && b[i-1][j]=='h') return false;
+        if (i+l<b.length && b[i+l][j]=='h') return false;
+        if (j<b.length-1) {
+            if (i>0 && b[i-1][j+1]=='h') return false;
+            for (int k=0;k<l;k++) {
+                if (b[i+k][j+1]=='h') return false;
             }
+            if (i+l<b.length && b[i+l][j+1]=='h') return false; 
         }
-        y++;
-        if (y<b.length)  {
-            for (int k=0;k<z;k++) {
-                if (b[x+k][y]=='h') return false;
-            }
+        for (int k=0;k<l;k++) {
+            if (b[i+k][j]=='m') return false;
         }
         return true;
     }
